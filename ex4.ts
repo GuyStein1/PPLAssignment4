c4ac306fd4391f3fa8dde99295c62a656c069ffd
GuyStein1 <guy.stt8@gmail.com>
2025-06-20 10:22:04 +0300

    Q1

--- ex4.ts
+++ ex4.ts
@@ -1,13 +1,30 @@
 //Q1
-export function all<T>(promises : Array<Promise<T>>) : Promise<Array<T>> {
+export function all<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
+  return new Promise<T[]>((resolve, reject) => {
+    const results: T[] = new Array(promises.length);
+    let resolvedCount = 0;
 
-  return new Promise<T[]>( (resolve, reject) => {
-    resolve([]);
-    //TODO
+    if (promises.length === 0) {
+      resolve([]);
+      return;
+    }
+
+    promises.forEach((p, index) => {
+      p.then((value) => {
+        results[index] = value;
+        resolvedCount++;
+
+        if (resolvedCount === promises.length) {
+          resolve(results);
+        }
+      }).catch((err) => {
+        reject(err);  // reject immediately on any failure
+      });
+    });
   });
 }
 
-  
+
 // Q2
 export function* Fib1() {
 	// @TODO
