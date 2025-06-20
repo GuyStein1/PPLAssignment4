c9aa6601ff5edd06d88f66d1c98ce357e183a30c
GuyStein1 <guy.stt8@gmail.com>
2025-06-20 10:35:56 +0300

    Q2

--- ex4.ts
+++ ex4.ts
@@ -27,12 +27,22 @@ export function all<T>(promises: Array<Promise<T>>): Promise<Array<T>> {
 
 // Q2
 export function* Fib1() {
-	// @TODO
-  return undefined;
+  let a = 1, b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
 }
 
-
 export function* Fib2() {
-	// @TODO
-  return undefined;
+  const sqrt5 = Math.sqrt(5);
+  const phi = (1 + sqrt5) / 2;
+  const psi = (1 - sqrt5) / 2;
+
+  let n = 1;
+  while (true) {
+    const fib = (Math.pow(phi, n) - Math.pow(psi, n)) / sqrt5;
+    yield Math.round(fib);
+    n++;
+  }
 }
