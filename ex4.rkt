848489566af5e977ba32548226b80b33a5316f70
GuyStein1 <guy.stt8@gmail.com>
2025-06-20 15:32:34 +0300

    Refactor division function to simplify error handling for division by zero

--- ex4.rkt
+++ ex4.rkt
@@ -155,12 +155,8 @@
 ;; Purpose: Division of real numbers
 (define //
   (lambda (x y)
-    (let ((den (head y)))
-      (if (= den 0)
-          (error "//: Division by zero")
-          (cons-lzl (/ (head x) den)
-                    (lambda () (// (tail x) (tail y))))))))
-
+    (cons-lzl (/ (head x) (head y))
+              (lambda () (// (tail x) (tail y))))))
 
 
 ;;; Q4.2.a
