b640531f9f048fd4cb69c48beb1619c6c57e1cdd
GuyStein1 <guy.stt8@gmail.com>
2025-06-21 14:56:11 +0300

    Q5.2

--- ex4.pl
+++ ex4.pl
@@ -40,8 +40,21 @@ book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 % You can add more facts.
 
 
+% Helper predicate: A <= B for Church numerals
+less_eq(zero, _).
+less_eq(s(A), s(B)) :-
+    less_eq(A, B).
+
 % Signature: max_list(Lst, Max)/2
-% Purpose: true if Max is the maximum church number in Lst, false if Lst is emoty.
+% Purpose: true if Max is the maximum church number in Lst, false if Lst is empty.
+max_list([], false).
+max_list([X], X).
+max_list([H | T], H) :-
+    max_list(T, M),
+    less_eq(M, H).
+max_list([H | T], M) :-
+    max_list(T, M),
+    less_eq(H, M).
 
 
 
@@ -52,13 +65,27 @@ book(the_lord_of_the_rings, t, f, s(s(s(s(s(s(zero))))))).
 
 % Signature: author_of_genre(GenreName, AuthorName)/2
 % Purpose: true if an author by the name AuthorName has written a book belonging to the genre named GenreName.
+author_of_genre(GenreName, AuthorName) :-
+    genre(GenreId, GenreName),
+    author(AuthorId, AuthorName),
+    book(_, AuthorId, GenreId, _).
 
 
 
 
-
-
+% Convert Church numerals to integers
+church_to_int(zero, 0).
+church_to_int(s(N), I) :-
+    church_to_int(N, I0),
+    I is I0 + 1.
 
 
 % Signature: longest_book(AuthorName, BookName)/2
 % Purpose: true if the longest book that an author by the name AuthorName has written is titled BookName.
+longest_book(AuthorName, BookName) :-
+    author(AuthorId, AuthorName),
+    findall((LengthInt, Title), (
+        book(Title, AuthorId, _, Length),
+        church_to_int(Length, LengthInt)
+    ), Pairs),
+    max_member((_, BookName), Pairs).
